83ab2e36de24065337b2c2d90ba690bbb44b7bcf
Pradyumn Pundir <pundirpradyumn25@gmail.com>
2023-03-19 16:36:28 -0400

    signup

---
+++ data/signUp.js
@@ -0,0 +1,15 @@
+import {ObjectId} from 'mongodb';
+import {members} from '../config/mongoCollections.js';
+
+const userCollection = await members();
+
+const signUpFunctions = {
+
+    async create(fname, lname, age, email, password) {
+        const finalPush = await userCollection.insertOne({fname, lname, age, email, password})
+        return ({_id: finalPush.insertedId.toString(), fname, lname, age, email, password})
+    }
+
+}
+
+export default signUpFunctions
