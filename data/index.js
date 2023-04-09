96546e61b223a14fb09f3a24deb64a77a0708f1e
joemarks <jmichaelmarks@tutanota.com>
2023-04-09 13:15:56 -0400

    resolving conflict

--- data/index.js
+++ data/index.js
@@ -1,7 +1,7 @@
-// import teamDataFunctions from "./team.js";
+import teamDataFunctions from "./team.js";
 import * as groupDataFunctions from "./groups.js";
 import userDataFunctions from "./user.js";
 
 export const userData = userDataFunctions;
 export const groupData = groupDataFunctions;
-// export const teamData = teamDataFunctions;
+export const teamData = teamDataFunctions;
