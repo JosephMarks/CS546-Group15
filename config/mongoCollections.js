543cce6a418760ab8bdb5b61ac1be7167e74046b
joemarks <jmichaelmarks@tutanota.com>
2023-04-09 13:10:03 -0400

    commiting group changes

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -17,3 +17,4 @@ const getCollectionFn = (collection) => {
 
 export const members = getCollectionFn("team");
 export const users = getCollectionFn("users");
+export const groups = getCollectionFn("groups");
--- data/index.js
+++ data/index.js
@@ -1,3 +1,7 @@
-import teamDataFunctions from './team.js';
+// import teamDataFunctions from "./team.js";
+import * as groupDataFunctions from "./groups.js";
+import userDataFunctions from "./user.js";
 
-export const userData = teamDataFunctions;
+export const userData = userDataFunctions;
+export const groupData = groupDataFunctions;
+// export const teamData = teamDataFunctions;
