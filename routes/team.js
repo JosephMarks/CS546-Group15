d215156e755948b6f783028521bae6c3b9fd2e68
joemarks <jmichaelmarks@tutanota.com>
2023-04-10 22:34:03 -0400

    misc formatting

--- routes/team.js
+++ routes/team.js
@@ -1,20 +1,27 @@
 import { Router } from "express";
 const router = Router();
-import { teamData } from "../data/index.js";
+import { teamData } from "../data/team.js";
 import { ObjectId } from "mongodb";
+import { team } from "../config/mongoCollections.js";
 
-router.route("/team").get(async (req, res) => {
+router.route("/").get(async (req, res) => {
   try {
-    let teamList = await teamData.getAll();
-    let displayArray = [];
-    for (let i = 0; i < groupList.length; i++) {
-      let { name } = groupList[i];
-      let { _id } = groupList[i];
-      let groupObject = { _id: _id, name: name };
-      displayArray.push(groupObject);
-    }
-    console.log(teamList);
-    res.render("./teaminfo");
+    let theTeam = await teamData.getAll();
+    console.log(theTeam);
+    //   console.log("hello");
+    //   let teamList = await teamData.getAll();
+    //   let displayArray = [];
+    //   for (let i = 0; i < groupList.length; i++) {
+    //     let { name } = groupList[i];
+    //     let { _id } = groupList[i];
+    //     let groupObject = { _id: _id, name: name };
+    //     displayArray.push(groupObject);
+    //   }
+    let image = "/public/static/images/joe.png";
+    res.render("./teaminfo", {
+      image: image,
+      team: theTeam,
+    });
   } catch (e) {
     res.sendStatus(500);
   }
