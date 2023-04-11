f8f55f50b084203ece27019eb8bf9170f9b7929f
joemarks <jmichaelmarks@tutanota.com>
2023-04-10 23:03:24 -0400

    clean up

--- routes/team.js
+++ routes/team.js
@@ -6,22 +6,6 @@ import { team } from "../config/mongoCollections.js";
 
 router.route("/").get(async (req, res) => {
   try {
-    // let theName;
-    // console.log("hello");
-    // let theTeam = await teamData.getAll();
-    // for (let i = 0; i < theTeam.length; i++) {
-    //   theName = theTeam[i]["name"];
-    // }
-    //   console.log("hello");
-    //   let teamList = await teamData.getAll();
-    //   let displayArray = [];
-    //   for (let i = 0; i < groupList.length; i++) {
-    //     let { name } = groupList[i];
-    //     let { _id } = groupList[i];
-    //     let groupObject = { _id: _id, name: name };
-    //     displayArray.push(groupObject);
-    //   }
-
     const team = [
       { name: "Pradyumn Pundir", image: "/public/static/images/joe2.png" },
       { name: "Ruobing Liu", image: "/public/static/images/joe2.png" },
