1a49828f549697cabdfe3212575abc426749afaa
tzuminglu <1994515daniel@gmail.com>
2023-04-14 19:36:19 -0400

    Network routes and handlebars

--- routes/network.js
+++ routes/network.js
@@ -21,14 +21,29 @@ router
 router.route('/post/:id')
     .get(async (req, res) =>
     {
-        const title = "Post"
-        const h1 = "Post";
-        const userPostList = await networkData.getPostByUserId(req.params.id);
-        const followerPostList = await networkData.getPostByConnections(req.params.id);
-        res.render('networks/networkPost', { title: title, h1: h1, userPost: userPostList, followerPost: followerPostList });
+        try
+        {
+            req.params.id = validation.checkId(req.params.id)
+        } catch(error)
+        {
+            return res.status(400).json({ error: error });
+        }
+        try
+        {
+            const userId = req.params.id
+            const title = "Post"
+            const h1 = "Post";
+            const userPostList = await networkData.getPostByUserId(req.params.id);
+            const followerPostList = await networkData.getPostByConnections(req.params.id);
+            res.render('networks/networkPost', { title: title, h1: h1, authorId: userId, userPost: userPostList, followerPost: followerPostList });
+        } catch(error)
+        {
+            return res.status(404).json({ error: error });
+        }
+
     })
 
-router.route('/post/yourpost/:id')
+router.route('/post/:id/postId/:id')
     .get(async (req, res) =>
     {
         const post = await networkData.getPostById(req.params.id);
@@ -37,6 +52,49 @@ router.route('/post/yourpost/:id')
         const h1 = post.content;
         res.render('networks/yourPost', { title: title, h1: h1, post: post, fname: author.fname, lname: author.lname });
     })
+    .post(async (req, res) =>
+    {
+        const updatedData = req.body;
+        let errors = [];
+        try
+        {
+            req.params.id = validation.checkId(req.params.id, 'ID url param');
+        } catch(e)
+        {
+            errors.push(e);
+        }
+        try
+        {
+            updatedData.newComment = validation.checkString(updatedData.newComment, 'Comment');
+        } catch(e)
+        {
+            errors.push(e);
+        }
+
+        if(errors.length > 0)
+        {
+            res.render(`post/postId/${req.params.id}`, {
+                errors: errors,
+                hasErrors: true,
+                newComments: updatedData,
+            });
+            return;
+        }
+
+        const user = await networkData.getCommentsByCommentId(req.params.id);  //cannot get userid??
+        try
+        {
+            const updatedPost = await networkData.addComments(req.params.id, user.Id, updatedData)
+            const post = await networkData.getPostById(req.params.id)
+            res.render(`networks/yourPost`, { title: title, h1: h1, post: post, fname: author.fname, lname: author.lname })
+        } catch(e)
+        {
+            let status = e[0];
+            let message = e[1];
+            return res.status(status).json({ error: message });
+        }
+    }
+    )
 
 router.route('/follower/:id').get(async (req, res) =>
 {
