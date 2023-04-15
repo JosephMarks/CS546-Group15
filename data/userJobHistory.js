72a11b1ada197bf725a3232604a817a79e06279a
joemarks <jmichaelmarks@tutanota.com>
2023-04-15 19:21:42 -0400

    updating job history

---
+++ data/userJobHistory.js
@@ -0,0 +1,13 @@
+import { ObjectId } from "mongodb";
+import * as groupData from "./groups.js";
+
+// Functinon to create the new group sub-document.
+export const create = async (userId, role) => {};
+
+export const getAll = async (userId) => {};
+
+export const updateTitle = async (groupId, eventId, title) => {};
+
+export const updateEventDate = async (groupId, eventId, eventDate) => {};
+
+export const remove = async (eventId) => {};
