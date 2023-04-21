b64279cdced0c1f97147b60811d6546fe5c56d1c
pradyumn25 <pundirpradyumn25@gmail.com>
2023-04-20 20:24:35 -0400

    conflictsResolved

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import { dbConnection } from "./mongoConnection.js";
-
-const getCollectionFn = (collection) => {
-  let _col = undefined;
-
-  return async () => {
-    if (!_col) {
-      const db = await dbConnection();
-      _col = await db.collection(collection);
-    }
-
-    return _col;
-  };
-};
-
-// NOTE: YOU WILL NEED TO CHANGE THE CODE BELOW TO HAVE THE COLLECTION(S) REQUIRED BY THE ASSIGNMENT
-
-export const team = getCollectionFn("team");
-export const users = getCollectionFn("users");
-export const company = getCollectionFn("company");
-export const groups = getCollectionFn("groups");
-export const network = getCollectionFn("network");
-export const skills = getCollectionFn("skills");
-=======
 import { dbConnection } from "./mongoConnection.js";
 
 const getCollectionFn = (collection) => {
@@ -47,4 +22,3 @@ export const groups = getCollectionFn("groups");
 export const network = getCollectionFn("network");
 export const skills = getCollectionFn("skills");
 export const socialPost = getCollectionFn("socialPost");
->>>>>>> main
