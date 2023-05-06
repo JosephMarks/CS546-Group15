f30b6c8c87a274082ed5d0428ed5fdebd1160855
pradyumn25 <pundirpradyumn25@gmail.com>
2023-05-06 18:17:57 -0400

    small update in update company

--- config/settings.js
+++ config/settings.js
@@ -1,8 +1,8 @@
 export const mongoConfig = {
 
-  serverUrl:
-    "mongodb+srv://pundirpradyumn25:Par56789@cluster0.drlwwjm.mongodb.net/test'",
-  // serverUrl: 'mongodb://127.0.0.1:27017',
+  // serverUrl:
+  //   "mongodb+srv://pundirpradyumn25:Par56789@cluster0.drlwwjm.mongodb.net/test'",
+  serverUrl: 'mongodb://127.0.0.1:27017',
 
   database: "webDevelopmentGroup15",
 };
