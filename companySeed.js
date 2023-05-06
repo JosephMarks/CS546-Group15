014e558634ba237ca24d0ff14a01333be82742e0
pradyumn25 <pundirpradyumn25@gmail.com>
2023-05-06 19:53:51 -0400

    signup fix

--- companySeed.js
+++ companySeed.js
@@ -4,7 +4,7 @@ import userFunctions from "./data/user.js"
 try {
 
     // companyFunctions.createCompany('Google2', 'google@google.com', 'tech', ['al', 'ak'], '12345', 'this is google', '1.png');
-    companyFunctions.createCompany('Barclays', 'Barclays@Barclays.com', 'chemistry', ['id'], '56789', 'this is barclays', '2.png');
+    //companyFunctions.createCompany('Barclays', 'Barclays@Barclays.com', 'chemistry', ['id'], '56789', 'this is barclays', '2.png');
     // companyFunctions.createCompany('facebook', 'facebook@facebook.com', 'tech', ['il'], '12345', 'this is facebook', '3.png');
     // companyFunctions.createCompany('adobe', 'adobe@adobe.com', 'tech', ['ny'], '67895', 'this is adobe', '4.png');
     // companyFunctions.createCompany('prudential', 'prudential@prudential.com', 'tech', ['nj'], '25000', 'this is prudential', '5.png');
